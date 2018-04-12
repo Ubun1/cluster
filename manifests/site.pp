8668dccb0fb540521caa4680af4f994b5a6dba04
nkretov <nkretov@croc.ru>
2018-04-12 12:32:17 +0300

    update

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,20 @@
 node default {
-  class { 'docker':
-    version => 'latest',
+  class { 'docker': version => 'latest' }
+  package { 'epel-release': ensure => 'installed' }
+  package { 'python34-pip': ensure => 'installed' }
+
+  package { ['docker-compose']:
+    ensure   => present,
+    provider => pip3,
   }
-  docker::image { 'postgres': }
+
+  file {'nginx.conf': 
+    path => "/tmp/nginx.conf",
+    content => "puppet:///data/configs/nginx.conf"
+  }
+
+  file {'docker-compose': path => '/tmp/docker-compose.yml', 
+  content => "puppet:///data/configs/docker-compose.yml"
+}
+  docker_compose { '/tmp/docker-compose.yml': ensure   => present }
 }
